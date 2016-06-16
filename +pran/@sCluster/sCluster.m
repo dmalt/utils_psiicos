56c4a87e9aa9c97868420548e4cadb4c7861c06e
Dmitriy Altukhov <dm.altukhov@ya.ru>
2016-06-16 02:48:25 -0400

    Removed +pran

--- +pran/@sCluster/sCluster.m
+++
@@ -1,7 +0,0 @@
-classdef sCluster < Cluster & pran.sInits
-	methods
-		function obj = sCluster(ConInds)
-			obj@Cluster(ConInds);
-		end
-	end
-end
--- +pran/@sConnections/sConnections.m
+++
@@ -1,7 +0,0 @@
-classdef sConnections < Connections & pran.sInits
-	methods
-		function obj = sConnections(ConInds)
-			obj@Connections(ConInds);
-		end
-	end
-end
