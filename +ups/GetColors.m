bac6ab6e57a72223b8404be0b4ee890b76cc3847
Dmitriy Altukhov <dm.altukhov@ya.ru>
2016-10-31 23:28:03 -0400

    Added extra color schemes

--- +ups/GetColors.m
+++ +ups/GetColors.m
@@ -1,4 +1,4 @@
-function colors = GetColors()
+function colors = GetColors(mode)
 % ------------------------------------------
 % Define and return nice colorscheme
 % ------------------------------------------
@@ -11,16 +11,42 @@ function colors = GetColors()
 %   colors.name - string; name of color;
 % ________________________________________
 % Dmitrii Altukhov, dm.altukhov@ya.ru
-
-	grey   = [112,127,127] / 256;
-	orange = [255 141 0]   / 256;
-	green  = [12 232 126]  / 256;
-	blue   = [0 35 255]    / 256;
-	violet = [174 12 232]  / 256;
-	yellow = [255 248 13]  / 256;
-	red    = [232 12 143]  / 256;
-	colors = struct('RGB',   {grey, orange, green, blue, violet, yellow, red}, ...
-					'name', {'grey' 'orange' 'green' 'blue' 'violet' 'yellow' 'red'});
+	if nargin < 1
+		mode = 1;
+	end
+	switch mode
+		case 1
+			color_scheme = {
+			    [112,127,127] / 256,   % black
+				[255 141 0]   / 256, % orange
+				[12 232 126]  / 256, % green
+				[0 35 255]    / 256, % blue
+				[174 12 232]  / 256, % violet
+				[255 248 13]  / 256, % yellow
+				[232 12 143]  / 256  % red
+			};
+		case 2
+			color_scheme = {
+				[242,237,217]    / 256,
+				[241, 169, 78]   / 256,
+				[68, 179, 194]   / 256,
+				[123, 141, 142]  / 256,
+				[93, 76, 70]     / 256,
+				[ 228, 86, 65]   / 256,
+				[232, 12, 143]   / 256
+			};
+		case 3
+			color_scheme = {
+				[242, 242, 242]  / 256,
+				[129, 67, 116]   / 256,
+				[81, 163, 157 ]  / 256,
+				[183, 105, 92]   / 256,
+				[205, 187, 121]  / 256,
+				[ 6, 66, 92]     / 256,
+				[232 12 143]     / 256
+			};
+	end
+	colors = struct('RGB', color_scheme);
 	% colors.names = {};
 end
  
