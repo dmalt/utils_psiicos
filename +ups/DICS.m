1305da44807d0f81e4ff4a29896249c87bfa562e
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-01-19 11:09:06 +0300

    Added docstring; added fast scan

--- +ups/DICS.m
+++ +ups/DICS.m
@@ -1,35 +1,52 @@
-function [A,Ps] = DICS(C,G, lambda)
+function [A, Ps, Cs, IND] = DICS(C, G, lambda)
 % -------------------------------------------------------
-% DICS beamformer on cross-spectrum
+% DICS beamformer on cross-spectrum. compute beamformer
+% topographies and use them to estimate powers and 
+% rank source-level connections by their correlation
+% with sensor-level cross-spectrum
 % -------------------------------------------------------
 % FORMAT:
-%  [A,Ps] = DICS(C,G)
+%  [A, Ps, Cs, IND] = DICS(C, G, lambda)
 % INPUTS:
-%   C        -
-%   G
+%   C        - {n_sensors x n_sensors} matrix;
+%              sensor-level cross-spectrum
+%   G        - {n_sensors x n_sources * 2} matrix;
+%              forward operator
 % OUTPUTS:
-%   A
-%   Ps
+%   A        - {n_sources * 2 x n_sensors} matrix;
+%              inverse operator
+%   Ps       - {n_sensors x 1} vector;
+%              source-level power estimates
+%   Cs       - {n_sources * (n_sources - 1) / 2 x 1} vector
+%              vectorized upper diagonal of matrix of
+%              source-level connection correlations with
+%              sensor-level cross-spectrum
+%   IND      - {n_sources * (n_sources - 1) / 2 x 2} matrix;
+%              mapping between linear and matrix index notation;
 % ________________________________________
 % Dmitrii Altukhov, dm.altukhov@ya.ru
+
+    import ps.PSIICOS_ScanFast
 
     if nargin < 3
         lambda = 10;
     end
 
     Nch = size(C,1);
-    iC = inv(C + lambda * trace(C) / Nch * eye(Nch));
-    Ns = fix(0.5 * size(G,2)); % assume tangent space dimension of 2 
+    C_reg = (C + lambda * trace(C) / Nch * eye(Nch));
+    Ns = fix(0.5 * size(G, 2)); % assume tangent space dimension of 2
 
     range = 1:2;
     A = zeros(size(G'));
 
     for i = 1:Ns
         L = G(:,range);
-        A(range,:) = inv(L' * iC * L) * L' * iC;
+        A(range,:) = (L' * C_reg * L) \ L' / C_reg;
         range = range + 2;
     end
 
+    % ---------- Estimate powers -------- %
+    Ps = zeros(Ns,1);
     for i=1:Ns
         range_i = i * 2 - 1 : i * 2;
         ai = A(range_i,:);
@@ -37,3 +54,8 @@ function [A,Ps] = DICS(C,G, lambda)
         [~, s, ~] = svd(cs);
         Ps(i) = sqrt(s(1,1));
     end;
+    % ----------------------------------- %
+
+    % Rank connections by their correlation with vec(C)
+    [Cs, IND] = PSIICOS_ScanFast(A', C(:));
+end
