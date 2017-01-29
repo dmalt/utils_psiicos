80e632b3ec406cb5372ea78553789592921b0693
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-01-29 18:42:29 +0300

    Using DICS with imag argument

--- +ups/GCS_DICS.m
+++ +ups/GCS_DICS.m
@@ -17,12 +17,12 @@ function [Cs, IND] = GCS_DICS(C,G,lambda)
     import ups.GCS_ScanFast
     import ups.DICS
 
-    Ns = fix(0.5 * size(G,2)); % assume tangent space dimension of 2 
+    Ns = fix(0.5 * size(G, 2)); % assume tangent space dimension of 2 
 
-    A = DICS(C,G,lambda);
+    A = DICS(C, G, lambda, false);
 
     Cs  = zeros(Ns * (Ns - 1) / 2, 1);
     IND = zeros(Ns * (Ns - 1) / 2, 2);
 
-    [Cs,IND] = GCS_ScanFast(C,G,A);
+    [Cs, IND] = GCS_ScanFast(C,G,A);
 end
