7f557bb9a8192a2f4bbff20aac9f22ea000fc6a7
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-05-22 16:20:54 +0300

    Added description and removed unnecessary commented out code

--- +ups/@Connections/Clusterize.m
+++ +ups/@Connections/Clusterize.m
@@ -1,13 +1,13 @@
 function obj = Clusterize(obj, clustSize, dPair)
-	import ups.PairwiseClust
-	import ups.CoOrientCluster
-		
-	% dPair = 0.02;
-	% clustSize = 2;
-	obj = obj.Merge();
-	obj.conInds = PairwiseClust(obj.conInds{1}, obj.headModel.GridLoc, dPair, clustSize);
+% ----------------------------------------------- 
+% Perform pairwise clustering on connections
+% ----------------------------------------------- 
+    import ups.PairwiseClust
+    import ups.CoOrientCluster
+
+    obj = obj.Merge();
+    obj.conInds = PairwiseClust(obj.conInds{1}, obj.headModel.GridLoc, dPair, clustSize);
     for iClust = 1:length(obj.conInds)
         obj.conInds{iClust} = CoOrientCluster(obj.conInds{iClust}, obj.headModel.GridLoc);
     end
-    % return obj
 end
