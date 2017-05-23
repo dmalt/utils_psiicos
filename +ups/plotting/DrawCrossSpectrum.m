82e66bdbdf94f264dce5324ed9735e0472173678
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-05-23 18:32:32 +0300

    moved plotting-related functions to plotting folder

--- +ups/plotting/DrawCrossSpectrum.m
+++ +ups/plotting/DrawCrossSpectrum.m
@@ -4,13 +4,13 @@ function CT_restored = DrawCrossSpectrum(CTimeseries, UP, ChLoc, ifSort)
 % reduced sensors, restore it back to real sensors,
 % if ifSort = true sort the rows of the resulting cross-
 % spectrum in order of ascendance of the distance between
-% two locations corresponding to this row and then plot 
+% two locations corresponding to this row and then plot
 % the resulting matrix
 % -------------------------------------------------------------
 % FORMAT:
-%   CT_restored = DrawCrossSpectrum(CTimeseries, UP, ChLoc, ifSort) 
+%   CT_restored = DrawCrossSpectrum(CTimeseries, UP, ChLoc, ifSort)
 % INPUTS:
-%   CTimeseries     - {N_sensors_reduced ^ 2 x Ntimes} 
+%   CTimeseries     - {N_sensors_reduced ^ 2 x Ntimes}
 %                     cross-spectrum matrix on reduced
 %                     sensors
 %   UP              - {N_sensors x N_sensors_reduced} matrix;
@@ -20,32 +20,33 @@ function CT_restored = DrawCrossSpectrum(CTimeseries, UP, ChLoc, ifSort)
 %                     coordinates in 3D-space
 %   ifSort          - boolean; if true perform sorting of the
 %                     cross-spectrum on real sensors in order
-%                     of ascendance of a distance between 
-%                     interacting locations corresponding 
+%                     of ascendance of a distance between
+%                     interacting locations corresponding
 %                     to a row.
 % OUTPUTS:
-%   CT_restored     - {N_sensors ^ 2 Ntimes} cross-spectrum 
+%   CT_restored     - {N_sensors ^ 2 Ntimes} cross-spectrum
 %                     matrix on real sensors
 % _____________________________________________________________
 % Dmitrii Altukhov, dm.altukhov@ya.ru
 
-	Nsensors = size(UP, 2);
-	Ntimes = size(CTimeseries, 2);
-	CTimeseriesRestored = zeros(Nsensors ^ 2, Ntimes); 
-	for t = 1:size(CTimeseries, 2)
-		CT_restored = RestoreCTdim(CTimeseries(:,t), UP);
-		CTimeseriesRestored(:,t) = CT_restored(:);
-	end
+    Nsensors = size(UP, 2);
+    Ntimes = size(CTimeseries, 2);
+    CTimeseriesRestored = zeros(Nsensors ^ 2, Ntimes);
+    for t = 1:size(CTimeseries, 2)
+        CT_restored = RestoreCTdim(CTimeseries(:,t), UP);
+        CTimeseriesRestored(:,t) = CT_restored(:);
+    end
 
-	if ifSort
-		sort_aux = zeros(Nsensors ^ 2, 1);
-		for iPair = 1:length(sort_aux)
-			[i,j] = ind2sub(Nsensors, iPair);
-			sort_aux(iPair) = sqrt((ChLoc(1,i) - ChLoc(1,j)) ^ 2 + ...
-								   (ChLoc(2,i) - ChLoc(2,j)) ^ 2 + ...
-								   (ChLoc(3,i) - ChLoc(3,j)) ^ 2);
-		end
-		[ind, key] = sort(sort_aux);
-		CTimeseriesRestored = CTimeseriesRestored(key,:);	
-	end
-	figure; imagesc (abs(CTimeseriesRestored));
+    if ifSort
+        sort_aux = zeros(Nsensors ^ 2, 1);
+        for iPair = 1:length(sort_aux)
+            [i,j] = ind2sub(Nsensors, iPair);
+            sort_aux(iPair) = sqrt((ChLoc(1,i) - ChLoc(1,j)) ^ 2 + ...
+                                   (ChLoc(2,i) - ChLoc(2,j)) ^ 2 + ...
+                                   (ChLoc(3,i) - ChLoc(3,j)) ^ 2);
+        end
+        [~, key] = sort(sort_aux);
+        CTimeseriesRestored = CTimeseriesRestored(key,:);
+    end
+    imagesc(abs(CTimeseriesRestored));
+end
