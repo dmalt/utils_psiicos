c19f407210d8c116c8dbb096949d147101fbaa23
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-11-06 16:20:16 +0300

    fix trailing whitespaces

--- +ups/+bst/LoadHeadModel.m
+++ +ups/+bst/LoadHeadModel.m
@@ -1,24 +1,24 @@
 function HM_ps = LoadHeadModel(subjID, condName, protocolPath, isLR, GainSVDTh)
 % -------------------------------------------------------
-% Load brainstorm and get grid locations matrix for 
-% subject subjID 
+% Load brainstorm and get grid locations matrix for
+% subject subjID
 % -------------------------------------------------------
 % FORMAT:
-%   HM_ps = LoadHeadModel(subjID, condName, protocolPath, isLR, GainSVDTh) 
+%   HM_ps = LoadHeadModel(subjID, condName, protocolPath, isLR, GainSVDTh)
 % INPUTS:
 %   subjID        - string; BST-imported subject name
 %   condName      - string; name of condition in BST protocol
 %   protocolPath  - string; absolute path to BST protocol
 %   isLR          - boolean; if true, take low resolution
 %                   cortex (default = true)
-%   GainSVDTh     - float; PVU threshold for PCA-driven 
+%   GainSVDTh     - float; PVU threshold for PCA-driven
 %                   sensor space reduction (default = 0.01)
 % OUTPUTS:
 %   HM_ps             - structure; PSIICOS head model;
 %                       forward model operator for reduced
 %                       sensor space and additional info.
 % -----------
-%   HM_ps.gain        - {nSenReduced x nSrc * 2} matrix of 
+%   HM_ps.gain        - {nSenReduced x nSrc * 2} matrix of
 %                       topographies for reduced sensor space
 %   HM_ps.UP          - {nSenReduced x nGradiometers} matrix
 %                       of transformation between reduced and
@@ -26,11 +26,11 @@ function HM_ps = LoadHeadModel(subjID, condName, protocolPath, isLR, GainSVDTh)
 %   HM_ps.subjID      - string; BST-imported subject name
 %   HM_ps.path        - string; path to BST head model
 %                       (file we load from)
-%   HM_ps.svdThresh   - float; PVU threshold for PCA-driven 
+%   HM_ps.svdThresh   - float; PVU threshold for PCA-driven
 %                       sensor space reduction
 %   HM_ps.GridLoc     - {nSrc x 3} matrix of source location
 %                       coordinates
-%   HM_ps.condName    - string; name of condtion in BST 
+%   HM_ps.condName    - string; name of condtion in BST
 %                       protocol
 % ________________________________________________________
 % Dmitrii Altukhov, dm.altukhov@ya.ru
@@ -42,13 +42,13 @@ function HM_ps = LoadHeadModel(subjID, condName, protocolPath, isLR, GainSVDTh)
     if nargin < 5
         GainSVDTh = 0.01;
     end
-    if nargin < 4 
+    if nargin < 4
         isLR = true;
     end
     if nargin < 3
         protocolPath = '~/PSIICOS_osadtchii';
     end
-    %-------------------------------------------%
+    % ------------------------------------------- %
 
     % ---------- load BST head model ------------ %
     hm_path = [protocolPath, '/data/', subjID];  % path to subject folder in protocol
@@ -61,7 +61,7 @@ function HM_ps = LoadHeadModel(subjID, condName, protocolPath, isLR, GainSVDTh)
         hm_path = [hm_path, '/', hmFolderName.name]; % path to HM_ps folder
     elseif ischar(condName)
         hm_path = [hm_path , '/', condName, '/'];
-    end 
+    end
     hmFiles = dir([hm_path, '/headmodel*.mat']); % available HM_ps files
     if isempty(hmFiles)
         error('LoadError:noFile', 'No head model files');
