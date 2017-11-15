510077f7da74399d30743f005b129fee513e52fe
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-11-15 15:50:31 +0300

    fix error with missing ends in newly added patchline

--- +ups/+plt/drawset.m
+++ +ups/+plt/drawset.m
@@ -146,32 +146,34 @@ function p = patchline(xs,ys,varargin)
 %
 % See also: patch, line, plot
 
-[zs,PVs] = parseInputs(varargin{:});
-if rem(numel(PVs),2) ~= 0
-    % Odd number of inputs!
-    error('patchline: Parameter-Values must be entered in valid pairs')
-end
+    [zs,PVs] = parseInputs(varargin{:});
+    if rem(numel(PVs),2) ~= 0
+        % Odd number of inputs!
+        error('patchline: Parameter-Values must be entered in valid pairs')
+    end
 
-% Facecolor = 'k' is (essentially) ignored here, but syntactically necessary
-if isempty(zs)
-    p = patch([xs(:);NaN],[ys(:);NaN],'k');
-else
-    p = patch([xs(:);NaN],[ys(:);NaN],[zs(:);NaN],'k');
-end
+    % Facecolor = 'k' is (essentially) ignored here, but syntactically necessary
+    if isempty(zs)
+        p = patch([xs(:);NaN],[ys(:);NaN],'k');
+    else
+        p = patch([xs(:);NaN],[ys(:);NaN],[zs(:);NaN],'k');
+    end
 
-% Apply PV pairs
-for ii = 1:2:numel(PVs)
-    set(p,PVs{ii},PVs{ii+1})
-end
-if nargout == 0
-    clear p
+    % Apply PV pairs
+    for ii = 1:2:numel(PVs)
+        set(p,PVs{ii},PVs{ii+1})
+    end
+    if nargout == 0
+        clear p
+    end
 end
 
 function [zs,PVs] = parseInputs(varargin)
-if isnumeric(varargin{1})
-    zs = varargin{1};
-    PVs = varargin(2:end);
-else
-    PVs = varargin;
-    zs = [];
+    if isnumeric(varargin{1})
+        zs = varargin{1};
+        PVs = varargin(2:end);
+    else
+        PVs = varargin;
+        zs = [];
+    end
 end
