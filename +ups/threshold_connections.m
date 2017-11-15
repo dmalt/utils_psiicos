f680c8ccf89cd88e604e4754193d65dcc6d8cb8b
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-11-16 00:51:42 +0300

    add docstring; remove unused variables

--- +ups/threshold_connections.m
+++ +ups/threshold_connections.m
@@ -1,16 +1,21 @@
 function con_inds = threshold_connections(Cs, thresh, IND)
 % -------------------------------------------------------
-% Return indices of connections that have subspace 
-% correlations are above a relative threshold
+% Return indices of connections that have subspace
+% correlations above a relative threshold
 % -------------------------------------------------------
 % FORMAT:
-%   con_inds = threshold_connections(Cs, thresh, IND) 
+%   con_inds = threshold_connections(Cs, thresh, IND)
 % INPUTS:
-%   Cs        -
-%   thresh
-%   IND
+%   Cs       - {n * (n - 1) / 2} vector
+%   thresh   - scalar; either float between 0 and 1
+%              or positive integer; in the former case
+%              return connections stronger than a relative
+%              threshold based on min and max values;
+%              in the latter case return thresh strongest
+%              connections
+%   IND      - {n * (n - 1) / 2 x 2} matrix
 % OUTPUTS:
-%   con_inds
+%   con_inds - {n x 2} matrix of indices
 % ________________________________________________________________________
 % Alex Ossadtchii ossadtchi@gmail.com, Dmitrii Altukhov, dm.altukhov@ya.ru
 
@@ -24,15 +29,11 @@ function con_inds = threshold_connections(Cs, thresh, IND)
         ind_threshold = find(Cs > corr_threshold);
     elseif thresh >= 1 && fix(thresh) == thresh
         thresh = int32(thresh);
-        [val, key] = sort(Cs, 'descend');
+        [~, key] = sort(Cs, 'descend');
         ind_threshold = key(1:thresh);
     end
 
-
-        
-    pair_max = IND(ind_threshold,:);
-
-    i = IND(ind_threshold, 1); 
+    i = IND(ind_threshold, 1);
     j = IND(ind_threshold, 2);
 
     con_inds(:,1) = i;
