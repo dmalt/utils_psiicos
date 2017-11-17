2cb9cbe997bbf2e92d42c9e47b0cc6556134b4eb
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-11-17 12:48:07 +0300

    retab; add docstring

--- +ups/@Connections/Merge.m
+++ +ups/@Connections/Merge.m
@@ -1,7 +1,17 @@
 function obj = Merge(obj)
+% ---------------------------------
+% Merge all sticks into one cluster
+% ---------------------------------
+% INPUT:
+%   NONE
+% OUTPUT:
+%   C    - Connections instance
+% _________________________________
+% AUTHOR: dmalt, dm.altukhov@ya.ru
+
     DipInd = [];
     for i = 1:length(obj.conInds)
-    	DipInd = [DipInd; obj.conInds{i}];
+        DipInd = [DipInd; obj.conInds{i}];
     end
     obj.conInds = {DipInd};
 end
