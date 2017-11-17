3abbe69b587e33bc4e21643eda3696933a4c6d6f
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-11-17 12:05:55 +0300

    retab

--- +ups/@Connections/ClustAndAvCells.m
+++ +ups/@Connections/ClustAndAvCells.m
@@ -3,8 +3,6 @@ function obj = ClustAndAvCells(obj, clustSize, dPair)
 % Custerize connections within each cell, average clusters
 % and save averaged connections in cells
 % -------------------------------------------------------
-% FORMAT:
-%   format 
 % INPUTS:
 %   inputs        -
 % OUTPUTS:
@@ -12,15 +10,16 @@ function obj = ClustAndAvCells(obj, clustSize, dPair)
 % ________________________________________
 % Dmitrii Altukhov, dm.altukhov@ya.ru
 
-	import ups.PairwiseClust
-		
-	dPair = 0.02;
-	clustSize = 2;
-	con_cel_clust = obj;	
-	for iCell = 1:length(obj.conInds) 
-		con_cel_clust.conInds = PairwiseClust(obj.conInds{iCell}, obj.headModel.GridLoc, dPair, clustSize);
-		con_cel_av = con_cel_clust.Average();
-		con_cel_merge = con_cel_av.Merge();
-		obj.conInds(iCell) = con_cel_merge.conInds();
-	end
+    import ups.PairwiseClust
+        
+    dPair = 0.02;
+    clustSize = 2;
+    con_cel_clust = obj;    
+    for iCell = 1:length(obj.conInds) 
+        con_cel_clust.conInds = PairwiseClust(obj.conInds{iCell}, obj.headModel.GridLoc,
+                                              dPair, clustSize);
+        con_cel_av = con_cel_clust.Average();
+        con_cel_merge = con_cel_av.Merge();
+        obj.conInds(iCell) = con_cel_merge.conInds();
+    end
 end
