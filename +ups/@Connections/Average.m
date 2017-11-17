f94c2bc277d7ce33ef6917c85fc4ef2f9777b4d2
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-11-17 12:40:10 +0300

    update docstring

--- +ups/@Connections/Average.m
+++ +ups/@Connections/Average.m
@@ -2,6 +2,13 @@ function obj = Average(obj)
 % ------------------------------------------
 % Spatially average clusters of connections
 % ------------------------------------------
+% INPUTS:
+%   NONE
+% OUTPUTS:
+%   C     - Connections instance
+% __________________________________________
+% AUTHOR: dmalt, dm.altukhov@ya.ru
+
     import ups.ClustAverage
 
     clustCenters = ClustAverage(obj.conInds, obj.headModel.GridLoc);
