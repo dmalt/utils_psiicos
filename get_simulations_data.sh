643c7209bafb16f9a5ee7ae4e02a1e6bee7660cb
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-12-12 18:04:58 +0300

    add script for simulations data downloads

---
+++ get_simulations_data.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+wget 'https://downloader.disk.yandex.ru/disk/d250817697a42a21690d1b5b4151fabc29cbe7610bfc443b71f78dabe56dc06e/5a3024c1/lxaiTboSYrnu-A9Dw49a7R86EF_D4NgwMfyZRJPLFc__lccYukvYeiJlJKyqz47IYTU20UmZBWp8tkcRusf1AQ%3D%3D?uid=0&filename=GLowRes.mat&disposition=attachment&hash=YxRv5U2EG04N16kf7NMVI3PwdIEXq40FCb5pm6fgN3A%3D%3A&limit=0&content_type=application%2Foctet-stream&fsize=10847428&hid=5ae0475f83443bedd760bab56d42a6b3&media_type=data&tknv=v2
+' -O 'GLowRes.mat'
+
+wget 'https://downloader.disk.yandex.ru/disk/bac00c5ac4a0945dafda3e452e85992bb97d2b225ff82507677c097c7983fd4b/5a30256b/lxaiTboSYrnu-A9Dw49a7VJ7N14A6etzN9_GToeTdfhzRNEWnZvgmSU_r1ceOWAciS4dqflEQ4TY-pTrmAKOFA%3D%3D?uid=0&filename=InputData4Simulations.mat&disposition=attachment&hash=wr/pd2T%2BBtxLv7i/OUouMfXgFNEW/X31EQPyYt2RMQk%3D%3A&limit=0&content_type=application%2Foctet-stream&fsize=156878338&hid=9d69da38c45072030fe01e5a625745d2&media_type=data&tknv=v2' -O 'InputData4Simulations.mat'
+
+wget 'https://downloader.disk.yandex.ru/disk/f9404b3b276770819b2f3f721f037717c05eaef7e85d80d56c227a690d298e85/5a302609/lxaiTboSYrnu-A9Dw49a7QJ4y4R0j34CpHFzukt7098fkj4LYzkPF0P59-NHaXfs9EPRNKboNz1mlWPYfIibJA%3D%3D?uid=0&filename=channel_vectorview306.mat&disposition=attachment&hash=JhpWhvoMiFNci%2B/vi3suR0ZBNV1AIZWnPEHCpzSi1Rc%3D%3A&limit=0&content_type=application%2Foctet-stream&fsize=46158&hid=8255aaad0690d73665f4ae684145362b&media_type=data&tknv=v2
+' -O 'channel_vectorview306.mat'
